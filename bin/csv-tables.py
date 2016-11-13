15a11b059937edc1b0acdc3c4a3ba3ec6cfd88a5
Kolen Cheung <christian.kolen@gmail.com>
2016-11-12 20:50:52 -0800

    Improve panflute csv-tables

--- bin/csv-tables.py
+++ bin/csv-tables.py
@@ -11,19 +11,38 @@ import panflute as pf
 
 def fenced_action(options, data, element, doc):
     # We'll only run this for CodeBlock elements of class 'csv'
-    title = options.get('title', 'Untitled Table')
-    title = [pf.Str(title)]
-    has_header = options.get('has-header', False)
+    try:
+        title = options.get('title')
+        # get has_header
+        has_header = options.get('has-header',True)
+        width = options.get('width')
+        alignment = options.get('alignment')
+    except AttributeError:
+        width = None
+        title = None
+        has_header = True
+        alignment = None
+
+    # get title
+    if title != None:
+        title = pf.convert_text(title)[0].content
+    # get width
+    if width != None:
+        width = [float(x) for x in width.split(",")]
+#     # get alignment
+#     if alignment != None:
+#         alignment = alignment.split(",")
 
     with io.StringIO(data) as f:
         reader = csv.reader(f)
         body = []
         for row in reader:
-            cells = [pf.TableCell(pf.Plain(pf.Str(x))) for x in row]
+            cells = [pf.TableCell(*pf.convert_text(x)) for x in row]
             body.append(pf.TableRow(*cells))
-
+    
+    # Todo: When has_header = False, it should return a list of empty row
     header = body.pop(0) if has_header else None
-    table = pf.Table(*body, header=header, caption=title)
+    table = pf.Table(*body, header=header, caption=title, width=width) #, alignment=alignment)
     return table
 
 
